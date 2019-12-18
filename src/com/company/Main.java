46948453ff704696bc6aa60c6c7bdd5205259f15
David <dbadikyan@gmail.com>
2019-12-18 22:26:52 +0400

    C2 Second commit adding scanner

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input your place: ");
+        String place = scanner.nextLine();
     }
 }
