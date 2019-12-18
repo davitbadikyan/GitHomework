6701a21d03fb178478e299d8e12c7619577388d1
Nonna Hakobyan <nonhakobyan11@gmail.com>
2019-12-18 23:05:40 +0400

    C9 calling FindPlace() function

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
+    private static String occupiedSpaces= "A1 H6 C3 D1 E4";
+    private static String place;
 
     public static void FindPlace(String enteredPlace) {
         place = enteredPlace;
@@ -19,5 +21,6 @@ public class Main {
         String place = scanner.nextLine();
         //Adding comment just for commit
         // Going just fine. Lets try another one
+        FindPlace(place);
     }
 }
