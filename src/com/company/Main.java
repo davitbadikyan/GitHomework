6d7754eee980d04237254aabd76cc168d6838de5
Nonna Hakobyan <nonhakobyan11@gmail.com>
2019-12-18 23:02:50 +0400

    C8 Adding FindPlace() function

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,15 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static void FindPlace(String enteredPlace) {
+        place = enteredPlace;
+        if (occupiedSpaces.contains(place)) {
+            System.out.println("This place is already occupied");
+        } else {
+            System.out.println("You can take it its free");
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Input your place: ");
